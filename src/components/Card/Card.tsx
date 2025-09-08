c6d04de632213424b3b6e79819ee967acbc90c72
BD <oliverluo0723@gmail.com>
2025-09-09 03:32:41 +0800

    feat: 新增技能卡片組件

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+import "./Card.css";
+import arrowRight from "@/assets/arrow-right.svg";
+
+interface Props {
+    title: string;
+    content: string;
+    badges: string[];
+    bgColor?: string;
+    link?: string;
+}
+
+export default function Card(prop: Props) {
+    const cardTitle = prop.title || "標題";
+    const cardContent = prop.content || "內容";
+    const cardLink = prop.link || "/about";
+    const badgeList = prop.badges || [];
+    return (
+        <article className="card" role="ariticle">
+            <div className="card-inner" style={{ backgroundColor: prop.bgColor || "" }}>
+                <div className="card-title">
+                    <h3>{cardTitle}</h3>
+                </div>
+                <div className="card-content">
+                    <p>{cardContent}</p>
+                </div>
+                <div className="card-badge-wrap" role="list">
+                    {badgeList.map((badge, index) => {
+                        return (
+                            <div className="badge" key={index}>
+                                {badge}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <footer className="card-bottom">
+                <p>探索更多</p>
+                <div className="act-button">
+                    <Link
+                        to={cardLink}
+                        aria-label={`探索更多關於${cardTitle}的內容`}
+                        title={`前往${cardTitle}詳細頁面`}
+                    >
+                        <img
+                            src={arrowRight}
+                            alt="前往網頁關於BD頁面"
+                            role="presentation"
+                            loading="lazy"
+                            width="24"
+                            height="24"
+                        />
+                    </Link>
+                </div>
+            </footer>
+        </article>
+    );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,6 +14,7 @@ import work4Img from "@/assets/imgs/img1.png";
 import { useRevealer } from "@/hooks/useRevealer.tsx";
 // 組件
 import Marquee from "@/pages/Home/components/Marquee.tsx"; // 跑馬燈
+import Card from "@/components/Card/Card";
 // CSS
 import "./Home.css";
 
@@ -65,21 +66,33 @@ export default function Home() {
             scrollTrigger: {
                 trigger: ".heroImg",
                 start: "top bottom",
-                end: "top center",
+                end: "top 50%",
                 scrub: 1,
             },
         });
 
         gsap.from(".work", {
+            yPercent: 15,
             scale: 0.8,
-            y: 150,
             autoAlpha: 0,
-            // duration: 1.5,
             ease: "power3.inOut",
             scrollTrigger: {
                 trigger: ".work",
                 start: "top 80%",
-                end: "top 30%",
+                end: "top 40%",
+                scrub: 1,
+            },
+        });
+
+        gsap.from(".core-wrap", {
+            yPercent: 10,
+            scale: 0.8,
+            autoAlpha: 0,
+            ease: "power3.inOut",
+            scrollTrigger: {
+                trigger: ".core-wrap",
+                start: "top 90%",
+                end: "top 50%",
                 scrub: 1,
             },
         });
@@ -102,7 +115,7 @@ export default function Home() {
                 <Marquee words="Scroll down to see magic " />
             </div>
             {/* 作品 */}
-            <section className="work-wrap w-full min-h-svh mt-4  grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <section className="work-wrap w-full min-h-svh mt-4 grid grid-cols-1 lg:grid-cols-2 gap-8 justify-center">
                 <div className="h-[400px] lg:h-[650px] bg-pink-400 work">
                     <img loading="lazy" src={work1Img} alt="" />
                     <h2>Listen 2023.04.07</h2>
@@ -120,6 +133,35 @@ export default function Home() {
                     <h2>Listen 2023.04.07</h2>
                 </div>
             </section>
+            {/* 核心能力 */}
+            <section className="core-wrap w-full mt-12 card-wrap grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8 justify-center">
+                <Card
+                    title="前端工程"
+                    content="打造直覺且流暢的數位介面"
+                    badges={[
+                        "Typescript",
+                        "SEO最佳化",
+                        "網頁無障礙(A11Y)",
+                        "RWD響應式設計",
+                        "SSR / SSG / SPA",
+                        "懶加載與打包優化",
+                    ]}
+                />
+                <Card
+                    title="UI/UX"
+                    content="以人為本，構建順暢且沉浸的數位體驗。"
+                    bgColor="#FFE1CC"
+                    badges={["Figma", "使用者旅程地圖", "Fitts Law", "Microinteractions", "漸進式揭露資訊"]}
+                />
+
+                <Card
+                    title="DevOps"
+                    content="透過自動化與可觀察性，實現穩定順暢的服務交付。"
+                    bgColor="#E2DBFA"
+                    badges={["CI/CD", "Linux三大觀察指標", "角色權限控制", "容器化", "Git版本控制"]}
+                />
+            </section>
+            <div className="w-full h-screen"></div>
         </>
     );
 }
