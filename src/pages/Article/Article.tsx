951eb29466a6173067fc59d03f75e6bd0ae6addb
BD <oliverluo0723@gmail.com>
2025-09-09 01:08:00 +0800

    feat: 安裝@lottiefiles/dotlottie-react

--- src/pages/Article/Article.tsx
+++ src/pages/Article/Article.tsx
@@ -1,12 +1,17 @@
+// hooks
 import { useRevealer } from "@/hooks/useRevealer.tsx";
-
+// lottie
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import catLootie from "@/assets/lotties/cat.lottie";
 export default function Article() {
     useRevealer();
     return (
         <>
             <div className="revealer"></div>
             <div className="w-full h-24"></div>
-            <div>我是文章</div>
+            <div className="w-48 h-48">
+                <DotLottieReact src={catLootie} autoplay loop style={{ width: "100%", height: "100%" }} />
+            </div>
         </>
     );
 }
